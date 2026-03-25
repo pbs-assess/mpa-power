e1c9d9d119f4bad8468ea4433cef71b594465d38
Sean Anderson <sean@seananderson.ca>
2026-03-25 14:57:20 -0700

    Fix typo

--- R/run-condition-sim-sample-fit.R
+++ R/run-condition-sim-sample-fit.R
@@ -1,4 +1,4 @@
-source("01-fit-conditioning-models.R")
+# source("R/01-fit-conditioning-models.R")
 source("R/02-generate-simulated-data.R")
 source("R/03-sample-simulated.R")
 source("R/04-fit-simulation.R")
